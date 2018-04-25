1aedf3552018fb8d7bb0c3f2ee6e8369696ff03d
John Wang <wycsasa@gmail.com>
2018-04-24 22:00:12 -0400

    gg

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,17 @@ const request = require("supertest");
 const {app} = require("../server");
 const {Todo} = require("../models/todo");
 
+const todos = [{
+	text: "First test to do"
+},{
+	text: "Second test to do"
+}];
+
 // make sure the db is empty
 beforeEach((done) => {
-	Todo.remove({}).then(() => done()); // empty the db
+	Todo.remove({}).then(() => {
+		Todo.insertMany(todos);
+	}).then(() => done()); // empty the db
 });
 
 describe("POST /todos", () => {
@@ -24,7 +32,7 @@ describe("POST /todos", () => {
 				if(err) { // anything above throws an error (err in response)
 					return done(err);
 				}							// check the DB side
-				Todo.find().then((todos) => { // there should be 1 item in DB
+				Todo.find({text}).then((todos) => { // there should be 1 item in DB
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -35,7 +43,7 @@ describe("POST /todos", () => {
 	it("should not create todo with invalid body data", (done) => {
 		var text = "";
 
-		request(app)  			
+		request(app)  			 
 			.post("/todos")			
 			.send({text})	
 			.expect(400)			// check for return status code
@@ -44,9 +52,22 @@ describe("POST /todos", () => {
 					return done(err);
 				}								// check the DB side
 				Todo.find().then((todos) => { // check for database
-					expect(todos.length).toBe(0);
+					expect(todos.length).toBe(2);
 					done();
 				}).catch((err) => done(err));
 			});
+	});
+});
+
+
+describe("GET /todos", () => {
+	it("shout get all todos", (done) => {
+		request(app)
+			.get("/todos")
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
 	});
 });
